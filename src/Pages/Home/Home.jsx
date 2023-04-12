de75bd20437da3a0b14b24ad4096852895ffd316
manishkcse <manish@roringo.com>
2023-04-12 14:14:38 +0530

    add third

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "../../Componets/Navbar/Navbar";
 import FirstImage from "./FirstImage/FirstImage";
 import SecondImage from "./SecondImage/SecondImage";
+import ThirdImage from "./ThirdImage/ThirdImage";
 
 const Home = () => {
   return (
     <div>
       <FirstImage />
-
       <SecondImage />
+      <ThirdImage />
     </div>
   );
 };
---
+++ src/Pages/Home/ThirdImage/ThirdImage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ThirdImage.css";
+
+const ThirdImage = () => {
+  return (
+    <div className="ThirdImage_Container">
+      <div className="ThirdImage_Section">
+        <p>
+          Siddham is a searchable database of 379 <span>OBJECTS</span> ,
+          together which contain 209
+          <span> INSCRIPTIONS</span> .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ThirdImage;
